e8c85d33a4fd490716db2b507b8a4f29d48becf8
scixing <whs11998@163.com>
2024-04-08 13:20:54 +0800

    update

--- testllm.py
+++ testllm.py
@@ -6,7 +6,7 @@ facellmt = facellm_test(api_key="EMPTY", openai_api_base="http://localhost:8000/
 sb = facellmt.get_storyboard_from_prompt("一个游戏主播的成长履历")
 # sb1 = facellmt.get_short_storyboard("一个游戏主播的成长履历, 由30帧构成")
 print(sb)
-
+ 
 
 prompt = change_to_animatediff_prompt(sb, 300)
 print(generate_animatediff_config(prompt))
